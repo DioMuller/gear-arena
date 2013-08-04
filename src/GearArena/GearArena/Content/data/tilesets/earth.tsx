30e244a12114ffb7dd855a510b4ea7a6ae56ab46
Diogo Muller <diogo.muller@live.com>
2013-08-04 20:20:29 -0300

    INT: Test map and tileset.

---
+++ src/GearArena/GearArena/Content/data/tilesets/earth.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Earth" tilewidth="4" tileheight="4">
+ <image source="../../images/tilesets/earth.png" trans="ff00ff" width="20" height="20"/>
+ <terraintypes>
+  <terrain name="Empty" tile="-1"/>
+  <terrain name="Ground" tile="-1"/>
+  <terrain name="Water" tile="-1"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,1"/>
+ <tile id="1" terrain="1,1,1,1"/>
+ <tile id="2" terrain="2,2,2,2"/>
+ <tile id="3" terrain="1,1,1,1"/>
+ <tile id="4" terrain="1,1,1,1"/>
+ <tile id="5" terrain="0,0,0,0"/>
+ <tile id="6" terrain="0,0,0,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,0,0,0"/>
+ <tile id="9" terrain="0,1,0,0"/>
+ <tile id="10" terrain="0,0,0,0"/>
+ <tile id="11" terrain="0,0,0,0"/>
+ <tile id="12" terrain="0,0,0,0"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14" terrain="1,0,0,0"/>
+ <tile id="15" terrain="0,0,0,1"/>
+ <tile id="16" terrain="0,0,1,0"/>
+ <tile id="17" terrain="0,0,0,1"/>
+ <tile id="18" terrain="0,0,1,0"/>
+ <tile id="19" terrain="0,0,0,1"/>
+ <tile id="20" terrain="0,1,0,0"/>
+ <tile id="21" terrain="1,0,0,0"/>
+ <tile id="22" terrain="0,0,0,1"/>
+ <tile id="23" terrain="0,0,1,0"/>
+ <tile id="24" terrain="0,0,1,0"/>
+</tileset>
