650fc0aa892e442e95cbb0b638ab311deb0eeb3d
Diogo Muller <diogo.muller@live.com>
2013-08-06 21:36:39 -0300

    FET: #2 - Create SolidBody Behavior.

---
+++ src/GearArena/GearArena/Behaviors/SolidBodyBehavior.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using MonoGameLib.Core.Entities;
+using MonoGameLib.Core.Extensions;
+
+namespace GearArena.Behaviors
+{
+    /// <summary>
+    /// Solid body physics behavior.
+    /// </summary>
+    class SolidBodyBehavior : Behavior
+    {
+        #region Properties
+        /// <summary>
+        /// Body momentum.
+        /// </summary>
+        public Vector2 Momentum { get; set; }
+
+        /// <summary>
+        /// Gravity being applied on the body.
+        /// </summary>
+        public Vector2 Gravity { get; set; }
+
+        /// <summary>
+        /// Constant forces applied on the body.
+        /// </summary>
+        public Dictionary<string, Vector2> ConstantForces { get; set; }
+
+        /// <summary>
+        /// Forces applied once on the body.
+        /// </summary>
+        public Stack<Vector2> Forces { get; set; }
+
+        /// <summary>
+        /// Body Mass.
+        /// </summary>
+        public float Mass { get; set; }
+
+        /// <summary>
+        /// Does the entity rotate with the force?
+        /// </summary>
+        public bool Rotate { get; set; }
+        #endregion Properties
+
+        #region Methods
+        /// <summary>
+        /// Updates the Rigid Body entity.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime)
+        {
+            Vector2 forces = Vector2.Zero;
+            float secs = (float)gameTime.ElapsedGameTime.TotalSeconds / 1000.0f; 
+
+            #region Calculate Forces
+            //Constant forces
+            foreach( Vector2 force in ConstantForces.Values )
+            {
+                forces += force;
+            }
+
+            //Forces applied once
+            while( Forces.Count > 0 )
+            {
+                forces += Forces.Pop();
+            }
+            #endregion Calculate Forces
+
+            Vector2 acceleration = (forces / Mass) + Gravity;
+            Vector2 accelSecs = acceleration / secs;
+
+            Entity.Position += (Momentum + accelSecs/2) * secs;
+            Momentum += accelSecs;
+
+            if( Rotate )
+            {
+                Entity.Rotation = Momentum.GetAngle();
+            }
+        }
+        #endregion Methods
+    }
+}
