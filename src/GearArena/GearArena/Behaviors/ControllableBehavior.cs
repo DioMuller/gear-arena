92b1e764f2d664833b54857aaccdda0fde20bac8
Diogo Muller <diogo.muller@live.com>
2013-08-08 21:46:06 -0300

    WIP: #3 - Create Controllable Behavior

---
+++ src/GearArena/GearArena/Behaviors/ControllableBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using MonoGameLib.Core.Entities;
+using MonoGameLib.Core.Input;
+
+namespace GearArena.Behaviors
+{
+    class ControllableBehavior : Behavior
+    {
+        #region Attributes
+        private GenericInput _input;
+        #endregion Attributes
+
+        #region Constructors
+        public ControllableBehavior(Entity parent, GenericInput input)
+            : base(parent)
+        {
+            _input = input;
+        }
+        #endregion Constructors
+
+        #region Methods
+        /// <summary>
+        /// Updates the Rigid Body entity.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime)
+        {
+            Entity.GetBehavior<SolidBodyBehavior>().ConstantForces["Accelerator"] = _input.LeftDirectional * 100f;
+        }
+        #endregion Methods
+    }
+}
--- src/GearArena/GearArena/Entities/Player.cs
+++ src/GearArena/GearArena/Entities/Player.cs
@@ -5,6 +5,7 @@ using System.Text;
 using GearArena.Behaviors;
 using Microsoft.Xna.Framework;
 using MonoGameLib.Core.Entities;
+using MonoGameLib.Core.Input;
 using MonoGameLib.Core.Sprites;
 
 namespace GearArena.Entities
@@ -15,7 +16,8 @@ namespace GearArena.Entities
         public Player() : base()
         {
             Behaviors.Add(new SolidBodyBehavior(this) { IsActive = true, Mass = 100, Rotate = false, Gravity = new Vector2(0f, 9.8f) } );
-            
+            Behaviors.Add(new ControllableBehavior(this, new GamepadInput(PlayerIndex.One)));
+
             Sprite = new Sprite("images/sprites/mecha.png", new Point(32, 32), 100);
 
             Sprite.Animations.Add( new Animation("idle", 0, 0, 0) );
